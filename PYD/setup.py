22b1993328eb17553cb8bd7ef81411887c4c16ba
Alem Haddush Fitwi <fitwialem2022@fb.com>
2023-04-18 23:51:30 -0700

    Update setup.py

--- PYD/setup.py
+++ PYD/setup.py
@@ -1,3 +1,26 @@
-#-------------------------------------------
-#
-#-------------------------------------------
+#----------------------------------------------------------------------
+# Steps To Create Pythonic DLL Known As PYD(Windows)/SO(Linux)
+#----------------------------------------------------------------------
+# Steps:
+"""
+  1. pip install cython
+  2. Create A Pythonic Program: test.py
+  3. Re-save it as test.pyx
+  4. Build The setup.py (python setup.py build_ext --inplace)
+      --> setup(ext_modules = cythonize('test.pyx')
+  5. Import the built pyd into your program (test.cp39-win_amd64.pyd <-- import test)
+
+"""
+
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+ext_modules = cythonize([
+                          Extension("test", ["test.pyx"]),
+                          #Extension("test1", ["test1.pyx"]),
+  
+                          #...
+  
+                          #Extension("testn", ["testn.pyx"]),
+  
+  setup(ext_modules = ext_modules)
